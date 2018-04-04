f7fbb7d66c5ec88cea008daffe8132c6a8958dd6
David Santiago Barrera <d4v1x.sb@gmail.com>
2018-04-04 23:09:33 +0100

    Fixed a problem with the injection of customer repository.

--- app/services/impl/CustomerServiceImpl.java
+++ app/services/impl/CustomerServiceImpl.java
@@ -1,5 +1,6 @@
 package services.impl;
 
+import com.google.inject.Inject;
 import models.CustomerResource;
 import org.jooq.DSLContext;
 import repositories.CustomerRepository;
@@ -11,6 +12,7 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
+    @Inject
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
